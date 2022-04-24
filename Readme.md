564271f72627d87a02d5c7bda47dc17a10a6dbd9
Sai Hari <saimorap@iu.edu>
2022-04-24 03:35:03 -0400

    P5 JS First Project

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# 10Print Generative ART using P5.js
+
+- My First Project in P5.js, coded the [10 print challenge](https://10print.org/).
+- The color mode is HSB, and color changes for every row.
+- The idea of color change is go through the full circle of Hue keeping Saturation and Brightness constants.
+
+## Rendered Image
+
+![10Print!](/photo.png)
---
+++ sketch.js
@@ -0,0 +1,38 @@
+let x = 0;
+let y = 0;
+let spacing = 30;
+let color = 0;
+
+function setup() {
+  // put setup code here
+  createCanvas(displayWidth, displayHeight);
+  background(0);
+}
+
+function draw() {
+  // put drawing code here
+  colorMode(HSB, 360, 100, 100);
+  stroke(color, 100, 100);
+  // stroke(255, 100, 100);
+
+  if (random(1) < 0.5) {
+    line(x, y, x + spacing, y + spacing);
+  } else {
+    line(x, y + spacing, x + spacing, y);
+  }
+
+  x = x + spacing;
+  if (x > width) {
+    x = 0;
+    y = y + spacing;
+    color = color + 360 / (displayHeight / spacing);
+  }
+}
+
+function mousePressed() {
+  print("The step of x is " + 360 / (displayHeight / spacing));
+  print("The color of x is " + color);
+  print("The displayWidth of x is " + displayWidth);
+  print("The displayHeight of x is " + displayHeight);
+  saveCanvas("photo", "png");
+}
